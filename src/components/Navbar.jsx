29743b561da48bad995f09a732a6d921fa69fee1
Matt Czencz <mattczenczdev@gmail.com>
2024-06-19 10:18:35 -0600

    Creates navbars, adds prettier formatting configs

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import cliqueLogo from '../assets/Logo.svg';
+import searchIcon from '../assets/search.svg';
+
+const Navbar = () => {
+  const utilityNavLinks = ['About Us', 'Careers', 'Contact Us'];
+  const primaryNavLinks = [
+    'Portfolio',
+    'Our Focus',
+    'The Blog',
+    'Partnerships',
+  ];
+
+  return (
+    <header>
+      {/* Utility navbar */}
+      <nav className="utility-nav">
+        <div className="angled-bg">
+          <ul className="nav-links">
+            {utilityNavLinks.map((link) => (
+              <li>
+                <a href="/">{link}</a>
+              </li>
+            ))}
+          </ul>
+          <form className="search-form">
+            <input type="search" placeholder="Search this site" />
+            <button type="submit">
+              <img src={searchIcon} alt="Search icon" />
+            </button>
+          </form>
+        </div>
+      </nav>
+
+      {/* Primary navbar */}
+      <nav className="primary-nav">
+        <a href="/">
+          <img src={cliqueLogo} alt="Clique branded logo" />
+        </a>
+
+        <div>
+          <ul className="nav-links">
+            {primaryNavLinks.map((link) => (
+              <li>
+                <a href="/">{link}</a>
+              </li>
+            ))}
+          </ul>
+          <button className="cta">Get started</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+export default Navbar;
