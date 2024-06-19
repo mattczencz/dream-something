e1d95a3cccab5b965aa84b3719547580411d80f5
Matt Czencz <mattczenczdev@gmail.com>
2024-06-19 14:40:32 -0600

    Adds specialties component and styling, splits out styles to separate files

--- src/components/AboutUsHeadline.jsx
+++ src/components/AboutUsHeadline.jsx
@@ -1,6 +1,6 @@
 const AboutUsHeadline = () => {
   return (
-    <div className="headline-container">
+    <div className="headline-container narrow-container">
       <h4 className="eyebrow">About us</h4>
       <h2>
         Lorem enim sed dignissim arcu leo mauris fermentum arcu sed. Vitae
--- src/components/Specialties.jsx
+++ src/components/Specialties.jsx
@@ -1,4 +1,56 @@
+import { FaArrowRight } from 'react-icons/fa6';
+import imageOne from '../assets/Images/image 4.jpg';
+import imageTwo from '../assets/Images/image 5.jpg';
+import imageThree from '../assets/Images/image 6.jpg';
+
 const Specialties = () => {
-  return <div>Specialties</div>;
+  return (
+    <div className="narrow-container specialties">
+      {/* Image Column */}
+      <div className="img-col">
+        <div className="image-wrapper img-1">
+          <img src={imageOne} alt="A person standing underneath a rock arch." />
+        </div>
+        <div className="image-wrapper img-2">
+          <img
+            src={imageTwo}
+            alt="A night time scene with a lake and a mountain in the distance."
+          />
+        </div>
+        <div className="image-wrapper img-3">
+          <img src={imageThree} alt="A beach sunrise." />
+        </div>
+      </div>
+
+      {/* Content Column */}
+      <div className="content">
+        <h3>Mi tellus magna at</h3>
+        <p className="sub-headline">
+          Aliquet eget purus eu nisl et eleifend. Arcu mi nulla odio.
+        </p>
+        <p>
+          Tristique et enim quisque gravida non parturient nunc a pharetra. Est
+          rhoncus porta faucibus sit nunc diam. Accumsan proin praesent faucibus
+          fermentum ipsum vulputate commodo viverra. Dui eget aliquam mauris
+          consectetur.
+        </p>
+
+        <div className="stats">
+          <h2>75+</h2>
+          <div className="stat-info">
+            <p className="eyebrow small">Large Statistic IPSUM</p>
+            <small>
+              Nisi a id sit pulvinar non. Quis tristique et sem cras morbi
+              lacus. Magna faucibus ultric.
+            </small>
+          </div>
+        </div>
+
+        <a href="/" className="cta">
+          Our specialty areas <FaArrowRight size={18} />
+        </a>
+      </div>
+    </div>
+  );
 };
 export default Specialties;
