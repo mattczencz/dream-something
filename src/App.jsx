3e9e99b94b4cf8fde155fe9a4f64722550e7f3a8
Matt Czencz <mattczenczdev@gmail.com>
2024-06-19 11:50:34 -0600

    Adds hero component, fixes styling on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <main>
+        <Hero />
+      </main>
     </>
   );
 }
